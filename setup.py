12018444f0da0e0a8c8e68332d96467cdcb94b04
Lars Lorch <lars.lorch@gmail.com>
2023-10-23 11:28:33 +0200

    Bump PyPI version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='avici',
-    version='1.0.3',
+    version='1.0.4',
     description='Amortized Inference for Causal Structure Learning',
     author='Lars Lorch',
     author_email='lars.lorch@inf.ethz.ch',
